661e3cb5c42379cd9a5e7e5ffc0991055b470ab0
alex <alex@pop-os.localdomain>
2021-04-18 11:36:52 +0700

    initial

---
+++ src/components/MyDialog.vue
@@ -0,0 +1,42 @@
+<template>
+<v-card>
+  <v-card-text>
+  This is my Dialog
+  <div>  value: {{value }} </div>
+  <div>  computed: {{computedValue}} </div>
+  <div>  watched: {{watchedResult}} </div>
+  </v-card-text>
+  <v-card-actions>
+  <v-btn @click="$emit('close')">Close</v-btn>
+  </v-card-actions>
+</v-card>
+</template>
+
+<script>
+export default {
+  name: "MyDialog",
+  props: {
+    value: {
+      type: String,
+    }
+  },
+  data: () => ({
+    watchedResult:''
+  }),
+  watch:{
+    value( val){
+      console.log('value has changed '+val)
+      this.watchedResult=val
+    }
+  },
+  computed:{
+    computedValue(){
+      return "computed" + this.value
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Parent.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container app>
+    <v-btn text @click="editWithNoTick">
+      Without Tick
+    </v-btn>
+
+    <v-btn  text @click="editWithTick">
+      With Tick
+    </v-btn>
+
+    <v-dialog
+        v-model="showDialog"
+        persistent
+    >
+      <MyDialog  :value="dialogValue" v-on:close="showDialog=false"></MyDialog>
+    </v-dialog>
+  </v-container>
+</template>
+
+<script>
+import MyDialog from "@/components/MyDialog";
+import Vue from 'vue'
+
+export default {
+  name: 'HelloWorld',
+  components:{MyDialog},
+  data: () => ({
+    dialogValue: '',
+    showDialog: false
+  }),
+  methods:{
+    editWithNoTick(){
+      console.log("edit without tick")
+      this.dialogValue='hello'
+      this.showDialog=true
+    },
+    editWithTick(){
+      console.log("edit with tick")
+      this.showDialog=true
+      this.dialogValue='xxx'
+      Vue.nextTick(() => this.dialogValue='hello')
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
